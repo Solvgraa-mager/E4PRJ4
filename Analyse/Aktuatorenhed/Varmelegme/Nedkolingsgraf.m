cd5f02162ff8f24ffb3bca8aee74698f6cb2a9b5
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2021-04-07 22:01:10 +0200

    Måling af akvarie

---
+++ Analyse/Aktuatorenhed/Varmelegme/Nedkolingsgraf.m
@@ -0,0 +1,20 @@
+%Nedkolingsgraf
+load('Input_26_25');
+load('Input');
+
+%% Plot
+plot(Input);
+xlabel('5 sekunder interval');
+ylabel('Grader');
+yline(36);
+yline(35);
+yline(34);
+yline(33);
+yline(32);
+yline(31);
+yline(30);
+yline(29);
+yline(28);
+yline(27);
+yline(26);
+yline(25);
