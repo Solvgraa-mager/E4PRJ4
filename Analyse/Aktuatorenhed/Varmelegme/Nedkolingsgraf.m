5cb23f3a71d8c149b54a5d7e84c9817bf2186764
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2021-05-12 20:25:00 +0200

    mere data

--- Analyse/Aktuatorenhed/Varmelegme/Nedkolingsgraf.m
+++ Analyse/Aktuatorenhed/Varmelegme/Nedkolingsgraf.m
@@ -47,12 +47,58 @@ clear all;
 % xlabel('Temperatur forskel i grader c')
 % ylabel ('enhed x')
 %% Plot
-plot(Plast_25L(725:length(Plast_25L)));
+%plot(Plast_25L(725:length(Plast_25L)));
 hold on
 plot(Plast_25L_21_Tilsat(1:length(Plast_25L_21_Tilsat)));
 title('Nedkølingsgraf af 25L vand med 35 PSU')
 xlabel('5 sekunder interval');
 ylabel('Grader');
 grid on
-
 hold off
+
+%%  Real
+X = double(blanks(length(Water_Heated)).');
+for i = 1:length(Water_Heated)
+   X(i) = Water_Heated(i)-2;
+end
+%plot(Water_Heated(210:length(Water_Heated)));
+plot(X(210:length(X)));
+title('opvarmning af 25L vand med 35 PSU 40 Watt ( 33.33% PWM)')
+xlabel('40 sekunder interval ( 1.1 time pr 100) ');
+ylabel('Grader');
+hold on
+grid on
+%%  Fake
+
+Y = double(blanks(length(WaterHeated_Fake)).');
+for i = 1:length(WaterHeated_Fake)
+   Y(i) = WaterHeated_Fake(i)-1;
+end
+plot(Y(210:length(Y)));
+%plot(WaterHeated_Fake(210:length(WaterHeated_Fake)));
+title('opvarmning af 25L vand med 35 PSU 24 Watt ( 20% PWM)')
+xlabel('40 sekunder interval ( 1.1 time pr 100) ');
+ylabel('Grader');
+grid on
+hold on
+
+
+%% FUCKED
+
+% Z = double(blanks(length(Fucked)).');
+% for i = 1:736
+%    Z(i) = Fucked(i)-0;
+% end
+% 
+% for i = 1400:length(Z)
+%    Z(i-737) = Fucked(i)-0;
+% end
+% 
+
+% plot(Z(1:length(Z)-(1400-736)));
+hold on
+plot(Fucked(1:length(Fucked)));
+title('opvarmning af 25L vand med 35 PSU 24 Watt ( 20% PWM)')
+xlabel('40 sekunder interval ( 1.1 time pr 100) ');
+ylabel('Grader');
+grid on
